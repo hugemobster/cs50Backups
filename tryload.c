018475a7e5eda554e0f58d12ff0b91e6c562a8cf
hugemobster <patelaryan05@gmail.com>
2024-05-28 09:02:49 +0000

    Tue, May 28, 2024, 9:02 AM +00:00

---
+++ tryload.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <strings.h>
+
+int LENGTH = 45;
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
+// TODO: Choose number of buckets in hash table
+const unsigned int N = 676;
+
+// Hash table
+node *table[N];
+
+char u_word[45];
+
+unsigned int SIZE = 0;
+
+int main(void){
+    char path[] = "dictionaries/";
+}
